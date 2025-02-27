239c63de5923bba3ee42c20d2e88108056514429
0xguizera <dattioficial@gmail.com>
2025-02-27 20:52:13 -0300

    Update Layout.tsx

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,1 +1,13 @@
+import { Box, Container } from '@chakra-ui/react';
+import { Navbar } from './Navbar';
 
+export function Layout({ children }) {
+  return (
+    <Box>
+      <Navbar />
+      <Container maxW="container.xl" py={8}>
+        {children}
+      </Container>
+    </Box>
+  );
+}
