697cd9c59ee686a1e51b95f3a380e7c7fb5bbea6
0xguizera <dattioficial@gmail.com>
2025-02-27 20:46:47 -0300

    Update sheets.ts

--- src/services/sheets.ts
+++ src/services/sheets.ts
@@ -1,1 +1,17 @@
+import { GoogleSpreadsheet } from 'google-spreadsheet';
 
+const SPREADSHEET_ID = process.env.NEXT_PUBLIC_SPREADSHEET_ID;
+const GOOGLE_SERVICE_ACCOUNT_EMAIL = process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL;
+const GOOGLE_PRIVATE_KEY = process.env.GOOGLE_PRIVATE_KEY;
+
+export async function getGoogleSheetsInstance() {
+  const doc = new GoogleSpreadsheet(SPREADSHEET_ID);
+  
+  await doc.useServiceAccountAuth({
+    client_email: GOOGLE_SERVICE_ACCOUNT_EMAIL!,
+    private_key: GOOGLE_PRIVATE_KEY!,
+  });
+  
+  await doc.loadInfo();
+  return doc;
+} 
