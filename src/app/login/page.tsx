812fc26a2dacea36b47a7bb6eb5b39df96dd7320
0xguizera <dattioficial@gmail.com>
2025-02-27 20:34:32 -0300

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,1 +1,17 @@
+'use client';
 
+import { Box, Container, VStack, Heading } from '@chakra-ui/react';
+import { LoginForm } from '@/components/auth/LoginForm';
+
+export default function LoginPage() {
+  return (
+    <Container maxW="container.sm" py={10}>
+      <VStack spacing={8}>
+        <Heading>Login</Heading>
+        <Box w="full">
+          <LoginForm />
+        </Box>
+      </VStack>
+    </Container>
+  );
+} 
