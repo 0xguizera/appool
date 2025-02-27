3d7b20cfcacfe8e1df03f08fa7a691cdf3f17c56
0xguizera <dattioficial@gmail.com>
2025-02-27 20:14:42 -0300

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import { Box, Container, VStack, Heading } from '@chakra-ui/react';
+import { RegisterForm } from '@/components/auth/RegisterForm';
+
+export default function RegisterPage() {
+  return (
+    <Container maxW="container.sm" py={10}>
+      <VStack spacing={8}>
+        <Heading>Registro</Heading>
+        <Box w="full">
+          <RegisterForm />
+        </Box>
+      </VStack>
+    </Container>
+  );
+} 
