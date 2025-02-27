9e0169dbf43c3331911eacd02bd1211d9c162d19
0xguizera <dattioficial@gmail.com>
2025-02-27 20:38:44 -0300

    Update index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,18 @@
-src/types/index.t
+export interface User {
+  uid: string;
+  userId: string;
+}
+
+export interface Posicao {
+  id: string;
+  userId: string;
+  tokenPrincipal: string;
+  tokenSecundario: string;
+  invest: number;
+  precoAbertura: number;
+  precoMin: number;
+  precoMax: number;
+  taxaAbertura: number;
+  status: 'MONTAGEM' | 'REMONTAGEM' | 'REMOVIDA';
+  timestamp: Date;
+} 
