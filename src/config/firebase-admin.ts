6e7880041a5fd7f76154045c4a83693afcd3f7c0
0xguizera <dattioficial@gmail.com>
2025-02-27 20:49:51 -0300

    Update firebase-admin.ts

--- src/config/firebase-admin.ts
+++ src/config/firebase-admin.ts
@@ -1,1 +1,18 @@
+import { initializeApp, cert } from 'firebase-admin/app';
+import { getFirestore } from 'firebase-admin/firestore';
 
+if (!process.env.FIREBASE_ADMIN_PRIVATE_KEY) {
+  throw new Error('Firebase Admin Private Key não encontrada');
+}
+
+const serviceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY.replace(/\\n/g, '\n'),
+};
+
+const app = initializeApp({
+  credential: cert(serviceAccount)
+});
+
+export const adminDb = getFirestore(app); 
